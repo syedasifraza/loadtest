7033bad332578f2e7c3883334d987095af79280b
Asif Raza <syedasif120683@gmail.com>
2024-12-03 11:36:21 -0600

    iperf3 command added

--- psheduler_job.sh
+++ psheduler_job.sh
@@ -2,4 +2,4 @@
 # Sleep for 30 seconds
 sleep 30
 # Execute the pscheduler command using the environment variable
-pscheduler task throughput --dest "$INPUT_MESSAGE"
+iperf3 -c "$INPUT_MESSAGE" -p 15005 -t 30
